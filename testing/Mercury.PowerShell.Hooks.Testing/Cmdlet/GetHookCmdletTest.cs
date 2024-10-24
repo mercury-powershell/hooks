30f4ac079d08ab7f1a0b9b3167a35e628f4c5d19
Bruno Sales <me@baliestri.dev>
2024-10-23 21:37:00 -0300

    add cmdlets tests

---
+++ testing/Mercury.PowerShell.Hooks.Testing/Cmdlet/GetHookCmdletTest.cs
@@ -0,0 +1,65 @@
+// Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Management.Automation;
+using Mercury.PowerShell.Hooks.Cmdlets;
+using Mercury.PowerShell.Hooks.Core.ComplexTypes;
+using Mercury.PowerShell.Hooks.Core.Enums;
+using Mercury.PowerShell.Hooks.Exceptions;
+
+namespace Mercury.PowerShell.Hooks.Testing.Cmdlet;
+
+public sealed class GetHookCmdletTest {
+  [Test]
+  public void OutputType_When_IdentifierIsNotProvided_ShouldBe_HookStore() {
+    // Arrange
+    _ = InvokationHelper.Invoke<RegisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", Guid.NewGuid().ToString())
+      .WithParameter("Action", ScriptBlock.Create("""{ Write-Host "Current Directory: $PWD" }""")));
+
+    // Act
+    var output = InvokationHelper.Invoke<GetHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory));
+
+    // Assert
+    Assert.That(output, Is.Not.Null);
+    Assert.That(output.BaseObject, Is.InstanceOf<HookStore>());
+  }
+
+  [Test]
+  public void OutputType_When_IdentifierIsKnown_ShouldBe_HookStoreItem() {
+    // Arrange
+    var identifier = Guid.NewGuid().ToString();
+    _ = InvokationHelper.Invoke<RegisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier)
+      .WithParameter("Action", ScriptBlock.Create("""{ Write-Host "Current Directory: $PWD" }""")));
+
+    // Act
+    var output = InvokationHelper.Invoke<GetHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier));
+
+    // Assert
+    Assert.That(output, Is.Not.Null);
+    Assert.That(output.BaseObject, Is.InstanceOf<HookStoreItem>());
+  }
+
+  [Test]
+  public void OutputType_When_IdentifierIsUnknown_ShouldBe_ErrorRecord() {
+    // Act
+    var identifier = Guid.NewGuid().ToString();
+    var errorRecord = InvokationHelper.ErrorRecord<GetHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier));
+
+    // Assert
+    Assert.That(errorRecord, Is.Not.Null);
+    Assert.Multiple(() => {
+      Assert.That(errorRecord.Exception, Is.InstanceOf<IdentifierNotFoundException>());
+      Assert.That(errorRecord.Exception.Message,
+        Is.EqualTo($"The hook with identifier '{identifier}' was not found in the 'ChangeWorkingDirectory' hook store."));
+    });
+  }
+}
---
+++ testing/Mercury.PowerShell.Hooks.Testing/Cmdlet/UnregisterHookCmdletTest.cs
@@ -0,0 +1,68 @@
+// Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Management.Automation;
+using Mercury.PowerShell.Hooks.Cmdlets;
+using Mercury.PowerShell.Hooks.Core.ComplexTypes;
+using Mercury.PowerShell.Hooks.Core.Enums;
+using Mercury.PowerShell.Hooks.Exceptions;
+
+namespace Mercury.PowerShell.Hooks.Testing.Cmdlet;
+
+public sealed class UnregisterHookCmdletTest {
+  [Test]
+  public void OutputType_When_IdentifierIsUnknown_ShouldBe_ErrorRecord() {
+    // Act
+    var identifier = Guid.NewGuid().ToString();
+    var errorRecord = InvokationHelper.ErrorRecord<UnregisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.PrePrompt)
+      .WithParameter("Identifier", identifier)
+      .WithParameter("PassThru", true));
+
+    // Assert
+    Assert.That(errorRecord, Is.Not.Null);
+    Assert.Multiple(() => {
+      Assert.That(errorRecord.Exception, Is.InstanceOf<IdentifierNotFoundException>());
+      Assert.That(errorRecord.Exception.Message,
+        Is.EqualTo($"The hook with identifier '{identifier}' was not found in the 'PrePrompt' hook store."));
+    });
+  }
+
+  [Test]
+  public void OutputType_When_IdentifierIsKnown_ShouldBe_HookStoreItem() {
+    // Arrange
+    var identifier = Guid.NewGuid().ToString();
+    _ = InvokationHelper.Invoke<RegisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier)
+      .WithParameter("Action", ScriptBlock.Create("""{ Write-Host "Current Directory: $PWD" }""")));
+
+    // Act
+    var output = InvokationHelper.Invoke<UnregisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier)
+      .WithParameter("PassThru", true));
+
+    // Assert
+    Assert.That(output, Is.Not.Null);
+    Assert.That(output.BaseObject, Is.InstanceOf<HookStoreItem>());
+  }
+
+  [Test]
+  public void OutputType_When_PassThruIsNotPresent_ShouldBe_Null() {
+    // Arrange
+    var identifier = Guid.NewGuid().ToString();
+    _ = InvokationHelper.Invoke<RegisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier)
+      .WithParameter("Action", ScriptBlock.Create("""{ Write-Host "Current Directory: $PWD" }""")));
+
+    // Act
+    var output = InvokationHelper.Invoke<UnregisterHookCmdlet>(configure => configure
+      .WithParameter("Type", HookType.ChangeWorkingDirectory)
+      .WithParameter("Identifier", identifier));
+
+    // Assert
+    Assert.That(output, Is.Null);
+  }
+}
